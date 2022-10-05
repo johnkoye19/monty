04b3699e918d56925662a0bbb018755c9b11984a
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-10-05 15:41:55 +0000

    d

--- monty_com.c
+++ monty_com.c
@@ -15,4 +15,25 @@ void pall(stack_t *h)
 
 stack_t *push(int n)
 {
+	/*add the number to the end*/
+	stack_t *h;
+
+	addnode_start(h, n);
+
+	return (h);
+}
+
+stack_t *addnode_start(stack *h, int n)
+{
+	stack_t *tmp = h, new;
+
+	if (h == 0)
+		return (0);
+	new = malloc(sizeof(stack_t));
+	if (new == 0)
+		return (0);
+	new->n = n;
+	new->prev = 0;
+	new->next = 
+
 }
