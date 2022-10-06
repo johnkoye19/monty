ea41f94efc4e27a24a040de7c3e8e66ded5c20a3
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-10-06 15:52:04 +0100

    Update monty_com.c

--- monty_com.c
+++ monty_com.c
@@ -59,3 +59,38 @@ stack_t *addnode_start(stack_t *h, const int n)
 	/* then craete node */
 	/* return new node */
 }
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - adds a node to the end of a linked list
+ * @head: double pointer to the beginning of the linked list
+ * @n: value to add to new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	 *new, *tmp;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
+}
